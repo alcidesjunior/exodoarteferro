377c42cf61e6081b2d2230e4604c08cf9c85ac21
Alcides Junior <alcidesjunior.infor@gmail.com>
2015-12-27 17:51:09 -0300

    exodo 2.0

---
+++ js/includes.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#menu').load('includes/menu.include.html');
+    $('#css').load('includes/css.include.html');
+    $('#js').load('includes/javascript.include.html');
+ });
