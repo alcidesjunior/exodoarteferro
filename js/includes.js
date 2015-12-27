1a6517eb414470b3c7db28ab382d0ec9412c0aab
Alcides Junior <alcidesjunior.infor@gmail.com>
2015-12-27 19:19:08 -0300

    nova aparencia do site

--- js/includes.js
+++ js/includes.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $('#menu').load('includes/menu.include.html');
     $('#css').load('includes/css.include.html');
     $('#js').load('includes/javascript.include.html');
+    $('#category-menu').load('includes/categories.include.html');
  });
