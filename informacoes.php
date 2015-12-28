2a6242e4deb999d677b61bc26bdcbaf638df8ee4
Amanda Tavares <amandatvt@hotmail.com>
2015-12-28 15:27:22 -0200

    Form e Info. Faltam mudanças

--- informacoes.php
+++ informacoes.php
@@ -14,13 +14,61 @@
     </div>
     <?php include "includes/includes.php";?>
     <div class="container-fluid padding-left-less">
+
         <div class="col-sm-9 glass pull-right">
-            <h1>O FORMULÁRIO DEVE FICAR AQUI</h1>
-            <p>você pode se basear no site antigo, lá tem informações que <br>
-            deverão estar nessa página além do formulário.<br>
-            Não esquece de colocar nossas informações aqui: MP Desenvolvimento tarará...<br>
-            Tem as infos no rodapé do site antigo. <h2>exodoarteferro.herobo.com</h2></p>
+            <h2>Informações para contato</h2>
+            <p><b>E-mail:</b><br>
+            exodoarteferro@gmail.com<br>
+            <br>
+
+            <b>Celular:</b><br>
+            OI (85) 98810-0430<br>
+            TIM (85) 99644-7453<br><br>
+
+            <p><b>Fortaleza, CE, Brasil</b></p>
+
+            <p><i class="fa fa-phone"></i> 
+                <abbr title="Celular">C</abbr>: OI (85) 98810-0430</p>
+            <p><i class="fa fa-phone"></i> 
+                <abbr title="Celular">C</abbr>: TIM (85) 99644-7453</p>
+            <p><i class="fa fa-envelope-o"></i> 
+                <abbr title="Email">E</abbr>: <a href="mailto:exodoarteferro@gmail.com">exodoarteferro@gmail.com</a>
+            </p>
+            <p><i class="fa fa-clock-o"></i> 
+                <abbr title="Horário de atendimento">H</abbr>: Domingo a Domingo: 8:00 às 22:00</p>
+
+            <div class="row">
+            <div class="col-sm-8">
+                <h3>Mande-nos uma mensagem</h3>
+                <form name="sentMessage" id="contactForm" action="sendmail.php" method="post">
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label>Nome:</label>
+                            <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
+                        </div>
+                    </div>
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label>Email:</label>
+                            <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
+                        </div>
+                    </div>
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label>Mensagem:</label>
+                            <textarea rows="6" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
+                        </div>
+                    </div>
+                    <div id="success"></div>
+                    <!-- For success/fail messages -->
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+
         </div>
-    </div>
+        <!-- /.row -->
+
+        </div> <!--col-9-->
+    </div> <!--container-->
 </body>
 </html>
