d4096d1c3d5c010de128ca12e182a8ccb9e98b09
Alcides Junior <alcidesjunior.infor@gmail.com>
2015-12-28 22:30:55 -0300

    -

--- includes/includes.php
+++ includes/includes.php
@@ -1,5 +1,5 @@
 <?php include 'includes/logo.include.html';?>
-<?php include 'includes/javascript.include.html';?>
+<?php include 'includes/javascript.html';?>
 <?php include 'includes/css.include.html';?>
 <?php include "includes/menu.include.html";?>
 <?php include 'includes/categories.include.html';?>
